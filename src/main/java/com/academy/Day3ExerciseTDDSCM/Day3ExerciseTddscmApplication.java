17b525729625979974b7cd26ddb0642c5958f17b
AlejandroAlinsangan <alejandro.alinsangan@softvision.com>
2022-11-11 20:39:46 +0800

    First Version

---
+++ src/main/java/com/academy/Day3ExerciseTDDSCM/Day3ExerciseTddscmApplication.java
@@ -0,0 +1,13 @@
+package com.academy.Day3ExerciseTDDSCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ExerciseTddscmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day3ExerciseTddscmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/Day3ExerciseTDDSCM/Day3ExerciseTddscmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.Day3ExerciseTDDSCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3ExerciseTddscmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
