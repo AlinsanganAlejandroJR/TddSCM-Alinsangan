fb2b2f104f16c4bd885e41ea132fa34b7191c29e
AlejandroAlinsangan <alejandro.alinsangan@softvision.com>
2022-11-11 20:45:17 +0800

    Added Repository, Employee Model, and Employee JSON File

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private int age;
+    private Double salary;
+    private String position;
+
+    public Employee(String name, int age, Double salary, String position) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.position = position;
+    }
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (age != employee.age) return false;
+        if (Double.compare(employee.salary, salary) != 0) return false;
+        if (!Objects.equals(name, employee.name)) return false;
+        return Objects.equals(position, employee.position);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        temp = Double.doubleToLongBits(salary);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (position != null ? position.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", position='" + position + '\'' +
+                '}';
+    }
+}
